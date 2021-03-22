931f8f2c4fe10c12fd6261b428c5e05c38546fb0
peter <ashu0000008@163.com>
2021-03-22 17:29:50 +0800

    add check eth balance

--- lib/balance.go
+++ lib/balance.go
@@ -5,7 +5,6 @@ import "C"
 import (
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
@@ -13,9 +12,9 @@ import (
 
 //export TestGetAccountBalance
 func TestGetAccountBalance() int64 {
-	ks := keystore.NewKeyStore("./keystore", keystore.StandardScryptN, keystore.StandardScryptP)
-	newAcc, _ := ks.NewAccount("Creation password")
-	fmt.Println(newAcc)
+	//ks := keystore.NewKeyStore("./keystore", keystore.StandardScryptN, keystore.StandardScryptP)
+	//newAcc, _ := ks.NewAccount("Creation password")
+	//fmt.Println(newAcc)
 
 	client, _ := ethclient.Dial("https://rinkeby.infura.io/v3/bebe1ba6263e44279370df5b205e8b9c")
 	account := common.HexToAddress("0xCC5d22bB804D7BFFc48BDAe7563Ba7843FE4928a")
